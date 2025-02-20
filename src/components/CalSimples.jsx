40d48fd9680665b762dc85f0444f0b5903bbc8a1
Nycholas Dev <143964335+SkyNicky@users.noreply.github.com>
2025-02-20 14:47:37 -0300

    Arrumando os símbolos CalSimples.jsx

--- src/components/CalSimples.jsx
+++ src/components/CalSimples.jsx
@@ -25,7 +25,8 @@ const Calculator = () => {
   const handleSolve = () => {
     try {
       let expression = input;
-
+        .replace(/×/g, "*") // Converte "×" para "*"
+        .replace(/÷/g, "/") // Converte "÷" para "/"
       // Substitui o símbolo √ por sqrt() para avaliação
       expression = expression.replace(/√(\d+)/g, "sqrt($1)");
 
@@ -54,12 +55,12 @@ const Calculator = () => {
         <button className="button" onClick={() => handleButtonClick("7")}>7</button>
         <button className="button" onClick={() => handleButtonClick("8")}>8</button>
         <button className="button" onClick={() => handleButtonClick("9")}>9</button>
-        <button className="button" onClick={() => handleButtonClick("/")}>/</button>
+        <button className="button" onClick={() => handleButtonClick("÷")}>/</button>
 
         <button className="button" onClick={() => handleButtonClick("4")}>4</button>
         <button className="button" onClick={() => handleButtonClick("5")}>5</button>
         <button className="button" onClick={() => handleButtonClick("6")}>6</button>
-        <button className="button" onClick={() => handleButtonClick("*")}>*</button>
+        <button className="button" onClick={() => handleButtonClick("×")}>*</button>
 
         <button className="button" onClick={() => handleButtonClick("1")}>1</button>
         <button className="button" onClick={() => handleButtonClick("2")}>2</button>
