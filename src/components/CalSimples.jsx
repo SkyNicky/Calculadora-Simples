2223d28b78dd4ec8e2125b2a36e5ad2546c56667
Nycholas Dev <143964335+SkyNicky@users.noreply.github.com>
2025-02-20 14:51:46 -0300

    Update CalSimples.jsx

--- src/components/CalSimples.jsx
+++ src/components/CalSimples.jsx
@@ -44,9 +44,9 @@ const Calculator = () => {
       <div className="result">{result}</div>
 
       <div className="buttons">
-        <button className="button" onClick={() => handleButtonClick("7")}>7</button>
-        <button className="button" onClick={() => handleButtonClick("8")}>8</button>
-        <button className="button" onClick={() => handleButtonClick("9")}>9</button>
+        <button className="button" onClick={() => handleButtonClick("1")}>1</button>
+        <button className="button" onClick={() => handleButtonClick("2")}>2</button>
+        <button className="button" onClick={() => handleButtonClick("3")}>3</button>
         <button className="button" onClick={() => handleButtonClick("÷")}>÷</button>
 
         <button className="button" onClick={() => handleButtonClick("4")}>4</button>
@@ -54,9 +54,9 @@ const Calculator = () => {
         <button className="button" onClick={() => handleButtonClick("6")}>6</button>
         <button className="button" onClick={() => handleButtonClick("×")}>×</button>
 
-        <button className="button" onClick={() => handleButtonClick("1")}>1</button>
-        <button className="button" onClick={() => handleButtonClick("2")}>2</button>
-        <button className="button" onClick={() => handleButtonClick("3")}>3</button>
+        <button className="button" onClick={() => handleButtonClick("7")}>7</button>
+        <button className="button" onClick={() => handleButtonClick("8")}>8</button>
+        <button className="button" onClick={() => handleButtonClick("9")}>9</button>
         <button className="button" onClick={() => handleButtonClick("+")}>+</button>
 
         <button className="button" onClick={() => handleButtonClick("0")}>0</button>
